b0d421642691749a4fda824cf9d91e96e5b11d9f
rajmahanty <raj.mahanty@happiestminds.com>
2018-01-07 13:58:30 +0000

    First Commit for Jar Packaging

---
+++ src/main/java/com/happiestminds/devops/App.java
@@ -0,0 +1,13 @@
+package com.happiestminds.devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!.....DevOps.." );
+    }
+}
